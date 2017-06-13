02b0323817240726e7768305ad0e2668fa468206
chocopie116 <chocopie116@gmail.com>
2017-06-13 14:32:38 +0900

    add softmaaaaaaaaaaaaaaax!

---
+++ chap3/3_5_softmax_overflow.py
@@ -0,0 +1,14 @@
+import numpy as np
+a = np.array([1010, 1000, 990])
+print(np.exp(a) / np.sum(np.exp(a)))
+
+c = np.max(a)
+print(a - c)
+
+np.exp(a-c) / np.sum(np.exp(a-c))
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y

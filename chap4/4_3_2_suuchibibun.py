36753c331497d1280c33175dbaf25921d290d8e4
chocopie116 <chocopie116@gmail.com>
2017-07-31 19:47:50 +0900

    点線ひいて、グラフのrangeを定めていい感じに出力されるように

--- chap4/4_3_2_suuchibibun.py
+++ chap4/4_3_2_suuchibibun.py
@@ -18,9 +18,21 @@ y = function_1(x)
 l.plt.xlabel("x")
 l.plt.ylabel("f(x)")
 
-tf = tangent_line(function_1, 5)
+x_point = 10
+
+# 傾きのグラフ
+tf = tangent_line(function_1, x_point)
 y2 = tf(x)
 
+# Range
+l.plt.xlim(0, 20)
+l.plt.ylim(-1, 6)
+
+# dashed
+l.plt.plot([0, x_point], [function_1(x_point), function_1(x_point)], '--o')
+l.plt.plot([x_point, x_point], [-1, function_1(x_point)], '--o')
+
+# Drawing
 l.plt.plot(x, y)
 l.plt.plot(x, y2)
 l.plt_show_alt(l.plt)

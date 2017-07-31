3b65b7e908c11bf2d08f17749ccf7d195ac76d54
chocopie116 <chocopie116@gmail.com>
2017-07-31 19:44:06 +0900

    傾きのグラフかいた

--- chap4/4_3_2_suuchibibun.py
+++ chap4/4_3_2_suuchibibun.py
@@ -3,10 +3,24 @@ from common import lib as l
 def function_1(x):
     return 0.01 * x**2 + 0.1*x
 
+def tangent_line(f, x):
+    d = numerical_diff(f, x)
+    y = f(x) - d*x
+    return lambda t: d*t + y
+
+def numerical_diff(f, x):
+    h = 1e-4 # 0.0001
+    return (f(x+h) - f(x-h)) / (2*h)
+
 x = l.np.arange(0.0, 20.0, 0.1)
 y = function_1(x)
 
 l.plt.xlabel("x")
 l.plt.ylabel("f(x)")
+
+tf = tangent_line(function_1, 5)
+y2 = tf(x)
+
 l.plt.plot(x, y)
+l.plt.plot(x, y2)
 l.plt_show_alt(l.plt)
